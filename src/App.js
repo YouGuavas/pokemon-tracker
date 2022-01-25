e7cf2faa23303d21e44317e9dc47e77ef8b76034
Patrick Yambrick <patrickyambrick@gmail.com>
2022-01-24 21:40:06 -0500

    Move Card img src out

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './Card';
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card src='https://www.popsockets.com/dw/image/v2/BFSM_PRD/on/demandware.static/-/Sites-popsockets-master-catalog/default/dw9eb9511a/images/hi-res/Poke-Ball-Gloss_01_Top-View.png?sw=800&sh=800' />
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import './Card.scss';
 
-export default function Card() {
+export default function Card(props) {
   return(
     <div className="card">
       <div className="card-inner">
         {/*Create the front of the card*/}
         <div className="card-front">
-          <img className="card-img" src="https://www.popsockets.com/dw/image/v2/BFSM_PRD/on/demandware.static/-/Sites-popsockets-master-catalog/default/dw9eb9511a/images/hi-res/Poke-Ball-Gloss_01_Top-View.png?sw=800&sh=800" />
+          <img className="card-img" src={props.src} />
         </div>
 
         {/*Create the back of the card*/}
