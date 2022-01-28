70eef54d3ecc1caea97e8de45bce6072b272e04d
Patrick Yambrick <patrickyambrick@gmail.com>
2022-01-27 19:46:24 -0500

    Add grid styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,13 @@ export default function Card(props) {
       <div className="card-inner">
         {/*Create the front of the card*/}
         <div className="card-front">
-          <img className="card-img" src={props.src} />
+          <img className="back-img" src={props.src} />
         </div>
 
         {/*Create the back of the card*/}
         <div className="card-back">
+
+          <img className="front-img" src="https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH6/SWSH6_EN_16.png" />
           
 
         </div>
