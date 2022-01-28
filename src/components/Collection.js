7e687070362bf6448367b96a9ca76185925e76e7
Patrick Yambrick <patrickyambrick@gmail.com>
2022-01-27 19:30:37 -0500

    Add Card grid to Collection component

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,7 +1,21 @@
+import '../styles/Collection.scss';
+
+import Card from './Card';
+
 export default function Collection(props) {
+  const cards = ['Grookey', 'Thwackey', 'Rillaboom'];
   return(
     <div className="collection grid">
       {props.collection}
+      {cards.map((item, index) => {
+        const gridRow = 1;
+        const gridColumn = index+1;
+        return(
+        <div key={index} className='item'>
+          <Card item={item} />
+        </div>
+        )
+      })}
     </div>
   )
 }
