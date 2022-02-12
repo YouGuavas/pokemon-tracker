47a9b2ebeeea9e1ec20f5ac16a07cabc2b0e501a
Patrick Yambrick <patrickyambrick@gmail.com>
2022-02-11 20:58:11 -0500

    Setup jwt

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const cors = require('cors');
 const db_tools = require('./db/connection');
