8bc1b9f5327b47c6f3ef251e15ca3fd1b3825826
Patrick Yambrick <patrickyambrick@gmail.com>
2022-02-26 20:16:47 -0500

    Fix deprecations and warnings

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -1,7 +1,5 @@
 
 import '../styles/Collection.scss';
-import { useState } from 'react';
-
 
 import Card from './Card';
 
@@ -31,7 +29,7 @@ export default function Collection(props) {
       {cards.map((item, index) => {
         return(
         <div key={index} className='item'>
-                  {cardsIHave.indexOf(index) != -1 ? 
+                  {cardsIHave.indexOf(index) !== -1 ? 
                   <Card data={{item, have:true}} addToCollection={addToCollection} removeFromCollection={removeFromCollection} count={count} total={cards.length} handleCount={props.setCount} index={index} src='https://www.popsockets.com/dw/image/v2/BFSM_PRD/on/demandware.static/-/Sites-popsockets-master-catalog/default/dw9eb9511a/images/hi-res/Poke-Ball-Gloss_01_Top-View.png?sw=800&sh=800' />
                   :
                   <Card data={{item, have:false}} addToCollection={addToCollection} removeFromCollection={removeFromCollection} count={count} total={cards.length} handleCount={props.setCount} index={index} src='https://www.popsockets.com/dw/image/v2/BFSM_PRD/on/demandware.static/-/Sites-popsockets-master-catalog/default/dw9eb9511a/images/hi-res/Poke-Ball-Gloss_01_Top-View.png?sw=800&sh=800' />
